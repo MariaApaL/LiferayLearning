54e6888679bad22c8e399123748e438015106e06
MariaApaL <mluquelopezwrk@gmail.com>
2022-12-15 13:38:15 +0100

    subiendo los modulos

---
+++ training-workspace/modules/training-portlet-diff/src/main/java/com/apa/liferay/training/portlet/diff/constants/TrainingMCVPortletDiffPortletKeys.java
@@ -0,0 +1,11 @@
+package com.apa.liferay.training.portlet.diff.constants;
+
+/**
+ * @author maria.luque.lopez
+ */
+public class TrainingMCVPortletDiffPortletKeys {
+
+	public static final String TRAININGMCVPORTLETDIFF =
+		"com_apa_liferay_training_portlet_diff_TrainingMCVPortletDiffPortlet";
+
+}
---
+++ training-workspace/modules/training-portlet/src/main/java/com/apa/liferay/training/portlet/constants/TrainingMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.apa.liferay.training.portlet.constants;
+
+/**
+ * @author maria.luque.lopez
+ */
+public class TrainingMVCPortletKeys {
+
+	public static final String TRAININGMVC =
+		"com_apa_liferay_training_portlet_TrainingMVCPortlet";
+
+}
